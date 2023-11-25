df55070fd92a2a5845ce5b9b0f8734c3ed1c4d7c
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-25 20:30:10 +0200

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,11 +1,20 @@
 // #1: we count with functions
-function multiplay(number_1, number_2) {
-  var number_3 = number_1 * number_2;
-  return number_3;
+// function multiplay(number_1, number_2) {
+//   var number_3 = number_1 * number_2;
+//   return number_3;
+// };
+
+// function add(number_3, number_4) {
+//     return number_3 + number_4
+// };
+
+// console.log(add(multiplay(36325, 9824), 777));
+
+function add (number_1, number_2) {
+  return number_1 + number_2;
 };
 
-function add(number_3, number_4) {
-    return number_3 + number_4 
+function multiplay(number_3, number_4) {
+  var number_1 = number_3 * number_4;
+  return number_1;
 };
-
-console.log(add(multiplay(36325, 9824), 777));
