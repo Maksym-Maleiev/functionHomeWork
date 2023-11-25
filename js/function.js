6effeac12eecb3468e29565a569968e10fd847c9
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-25 20:08:34 +0200

    Update function.js

--- js/function.js
+++ js/function.js
@@ -15,7 +15,7 @@ function multiplay(number_1, number_2) {
   return number_1 * number_2;
 };
 function add(number_3, number_4) {
-    var number_3 = number_1 * number_2;
+    var number_3 = multiplay;
     return number_3 + number_4;
 };
-console.log(multiplay, add);
+console.log(multiplay(36325, 9824));
