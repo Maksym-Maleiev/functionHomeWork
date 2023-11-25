12d5476baf37a19bcb6e2b9131eaeb36a0545ab3
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-25 20:24:01 +0200

    Update function.js

--- js/function.js
+++ js/function.js
@@ -4,9 +4,11 @@ function multiplay(number_1, number_2) {
   return number_3;
 };
 
- function add(number_3, number_4) {
-    return number_3 + number_4
+function add(number_3, number_4) {
+  if (number_3 === number_3) {
+    return number_3 + number_4 
+   }
 };
 
 console.log(multiplay(36325, 9824));
-// console.log(add(multiplay, 777));
+console.log(add(number_3, 777));
