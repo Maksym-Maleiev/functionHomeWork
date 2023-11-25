aedde2e949d778de67d4818db7fa318b1f8f9cb6
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-25 20:37:29 +0200

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,14 +1,16 @@
 // #1: we count with functions
-// function multiplay(number_1, number_2) {
-//   var number_3 = number_1 * number_2;
-//   return number_3;
-// };
+/*
+function multiplay(number_1, number_2) {
+  var number_3 = number_1 * number_2;
+  return number_3;
+};
 
-// function add(number_3, number_4) {
-//     return number_3 + number_4
-// };
+function add(number_3, number_4) {
+    return number_3 + number_4
+};
 
-// console.log(add(multiplay(36325, 9824), 777));
+console.log(add(multiplay(36325, 9824), 777));
+*/
 
 function add (number_1, number_2) {
   return number_1 + number_2;
@@ -21,4 +23,4 @@ function multiplay(number_3, number_4) {
 
 console.log(add(multiplay(36325, 9824), 777));
 
-// 2: 
+// 2: are these arrays the same?
