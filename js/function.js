57cbb59745b6d045e8a3af326f00dd07ae7657ee
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-25 19:41:34 +0200

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,10 +1,10 @@
 // #1: we count with functions
-var add = function (number_1, number_2) {
-    return number_1 + number_2;
+var multiplay = function (number_1, number_2) {
+  return number_1 * number_2;
 };
 
-var multiplay = function (number_3, number_4) {
-  return number_3 * number_4;
+var add = function (multiplay, number_3) {
+    return multiplay + number_3;
 };
 
 
