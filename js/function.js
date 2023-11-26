a3c5a6e4fe349fa6884e5d3c82bc209d23d7abe2
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-26 11:01:45 +0200

    Update function.js

--- js/function.js
+++ js/function.js
@@ -101,7 +101,6 @@ var updateGameState = function (guess, word, answerArray) {
 var showAnswerAndCongratulatePlayer = function (answerArray) {
   // We show the answer and welcome the player
   showPlayerProgress(answerArray);
-  alert(answerArray.join(" "));
   if (guess > 0) {
     alert("Good job! The answer was " + answerArray.join());
   } else {
