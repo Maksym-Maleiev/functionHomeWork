94f2b7df863d32a44e5449ac40d5e89b3ab810d1
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-26 10:29:43 +0200

    Update function.js

--- js/function.js
+++ js/function.js
@@ -84,6 +84,7 @@ var updateGameState = function (guess, word, answerArray) {
   for (var j = 0; j < word.length; j++){
     if (word[i] === guess && answerArray[j] === "_") {
       answerArray[i] = guess;
+      --remainingLetters;
     } 
   }
 };
